bdc83dbc8fc359cb088c11c2590e82f713a9f48c
ZetaRift <zetarift@gmail.com>
2019-06-10 00:40:49 -0400

    Now more interactive

--- counter.asm
+++ counter.asm
@@ -1,4 +1,4 @@
-; A counter, it dosen't show anything on the screen, so take note of addresses $0002 to $0004
+; A counter that actually shows something on the screen
 ; The counter is timed by the PPU's vblank so the loop is run through 60 times per second (On NTSC)
 ; Variable references:
 ; $0000 Sleep timer
@@ -6,7 +6,7 @@
 ; $0002 Hundredth counter
 ; $0003 Tenth counter
 ; $0004 Second counter
-; $0011 A press state (0-1)
+; $0011-$0018 Controller button state (A, B, Select, Start, Up, Down, Left, Right)
 ; $0200-$0203: Sprite 1 (Y pos, sprite index, sprite attribute, X pos)
 ; $0204-$0207: Sprite 2 (Y pos, sprite index, sprite attribute, X pos)
 ; $0208-$020B: Sprite 3 (Y pos, sprite index, sprite attribute, X pos)
@@ -26,7 +26,7 @@
 
   .bank 2
   .org $0000
-  .incbin "numbers.chr"
+  .incbin "numbers.chr" ; 8x8 sprite sheet
   .bank 1
   .org $E000
 PaletteData:
@@ -65,10 +65,10 @@ updatesprites: ; Update the sprite attributes before DMA transfer
   lda $0011
   sta $0202
 
-dmatransfer: ; Copy the sprite attributes to the PPU OAM (Object Attribute Memory)
+dmatransfer: ; Copy the sprite attributes from RAM to the PPU OAM (Object Attribute Memory)
   lda #$00
   sta $2003
-  lda #$02
+  lda #$02 ; Transfer data from $0200-$02FF to PPU OAM
   sta $4014
 
   lda #$0
@@ -158,13 +158,14 @@ loadspritesloop:
   cpx #$03
   bne loadspritesloop
 
+  lda #%00010000
+  sta $2001 ; Enable sprites
+
   lda #%10000000
   sta $2000 ; Allow PPU to call an NMI at vblank
 
-  lda #%00010000 ; enable sprites
-  sta $2001
 
-;;;;;;;;;;;;;;;; Put our initial sprites on the screen, this will update on the next vblank
+;;;;;;;;;;;;;;;; Put our initial sprites on the screen, this will show up on the next vblank
 
 ;;;;;;;;;;;;;;;; Sprite 1
   lda #$80 ; Sprite positioned on center of screen
@@ -202,13 +203,17 @@ vblankend:
   sta $0012
   sta $0013
   sta $0014
+  sta $0015
+  sta $0016
+  sta $0017
+  sta $0018
   jsr contcheck
   lda $0011
   cmp #$01
   beq resetall
   ldx $0001
   
-  cpx #59 ; Change to 50 if on PAL
+  cpx #59 ; Change to 49 if on PAL
   bcs countup
   
   ldx $0001
@@ -261,23 +266,151 @@ hundinc:
   stx $0002
 
   jmp loopend
-contcheck: ; Subroutine to check for controller button states
-  lda #$01 ; Strobe the controller so we can poll for pressed/released states
-  sta $4016
-  lda #$00
-  sta $4016 ; Avoiding a jsr + rts chain to save a few cycles
 
-  lda $4016
-  cmp #$40
-  bne apressed
-  rts
-apressed:
-  lda #$01
-  sta $0011
-  rts
 resetcounter: ; Resets counters
   ldx #$00
   stx $0002
   stx $0003
   stx $0004
   jmp loopend
+
+contcheck: ; Subroutine to check for controller button states sequentially, seems to be the most reliable so far
+  lda #$01 ; Strobe the controller so we can poll for pressed/released states
+  sta $4016
+  lda #$00
+  sta $4016 ; Avoiding a jsr + rts chain to save a few cycles
+
+ReadA:
+  lda $4016
+  and #%00000001 ; A button
+  bne apressed
+
+ReadB:
+  lda $4016 ; B
+  and #%00000001
+  bne bpressed
+
+ReadSel:
+  lda $4016 ; Select
+  and #%00000001
+  bne selpressed
+
+ReadSta:
+  lda $4016 ; Start
+  and #%00000001
+  bne stapressed
+
+ReadUp:
+  lda $4016 ; Up
+  and #%00000001
+  bne uppressed
+
+ReadDn
+  lda $4016 ; Down
+  and #%00000001
+  bne dwnpressed
+
+ReadLt:
+  lda $4016 ; Left
+  and #%00000001
+  bne ltpressed
+
+ReadRt:
+  lda $4016 ; Right
+  and #%00000001
+  bne rtpressed
+
+  rts
+apressed:
+  lda #$01
+  sta $0011
+  jmp ReadB
+
+bpressed:
+  lda #$01
+  sta $0012
+  jmp ReadSel
+
+selpressed:
+  lda #$01
+  sta $0013
+  jmp ReadSta
+
+stapressed:
+  lda #$01
+  sta $0014
+  jmp ReadUp
+
+uppressed:
+  lda #$01
+  sta $0015
+
+  ; Move our numbers up
+  ldx $0200
+  dex
+  stx $0200
+
+  ldx $0204
+  dex
+  stx $0204
+
+  ldx $0208
+  dex
+  stx $0208
+
+  jmp ReadDn
+
+dwnpressed:
+  lda #$01
+  sta $0016
+
+  ; Move our numbers down
+  ldx $0200
+  inx
+  stx $0200
+
+  ldx $0204
+  inx
+  stx $0204
+
+  ldx $0208
+  inx
+  stx $0208
+
+  jmp ReadLt
+
+ltpressed:
+  lda #$01
+  sta $0017
+
+  ; Move our numbers left
+  ldx $0203
+  dex
+  stx $0203
+
+  ldx $0207
+  dex
+  stx $0207
+
+  ldx $020B
+  dex
+  stx $020B
+  jmp ReadRt
+
+rtpressed:
+  lda #$01
+  sta $0018
+
+    ; Move our numbers right
+  ldx $0203
+  inx
+  stx $0203
+
+  ldx $0207
+  inx
+  stx $0207
+
+  ldx $020B
+  inx
+  stx $020B
+  rts
