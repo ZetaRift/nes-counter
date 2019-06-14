fc5fe73ca8a1934c2804870d22ca872199cc1963
ZetaRift <zetarift@gmail.com>
2019-06-13 20:25:57 -0400

    Minor variable update

--- counter.asm
+++ counter.asm
@@ -320,14 +320,14 @@ readjoy:
   ; While the strobe bit is set, buttons will be continuously reloaded.
  ; This means that reading from JOYPAD1 will only return the state of the
   ; first button: button A.
-  sta $4016
+  sta Joy1
   sta Joy1_buttons
   lsr a        ; now A is 0
   ; By storing 0 into JOYPAD1, the strobe bit is cleared and the reloading stops.
   ; This allows all 8 buttons (newly reloaded) to be read from JOYPAD1.
-  sta $4016
+  sta Joy1
 loop:
-  lda $4016
+  lda Joy1
   lsr a	       ; bit 0 -> Carry
   rol Joy1_buttons  ; Carry -> bit 0; bit 7 -> Carry
   bcc loop
